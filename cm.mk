946eab604c45badbe11b1bdc0a27ee2b20ec6bd3
Erik D <62072464+erikd256@users.noreply.github.com>
2021-12-31 14:36:44 +0100

    mkvendor.sh: Initial commit of t01

---
+++ cm.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := t01
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/acer/t01/device_t01.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := t01
+PRODUCT_NAME := cm_t01
+PRODUCT_BRAND := acer
+PRODUCT_MODEL := t01
+PRODUCT_MANUFACTURER := acer
